01818ae3a5166302bf95870a6cddfa624438259a
Hilary Titus Naor <hilarytn@gmail.com>
2023-07-18 14:49:08 -0700

    Update a document

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,6 +9,6 @@
 """
 
 def update_topics(mongo_collection, name, topics):
-"""changes all topics of a school document"""
+    """changes all topics of a school document"""
     return mongo_collection.update_many({"name": name},
                                         {"$set": {"topics": topics}})
