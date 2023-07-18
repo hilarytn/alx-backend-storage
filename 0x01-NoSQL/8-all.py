d30925bf6c7c7ef156f6a471b4d64e757087e783
Hilary Titus Naor <hilarytn@gmail.com>
2023-07-18 13:35:11 -0700

    Reformat comment

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-"""A function that lists all documents in a collection"""
-import pymongo
+"""A function that lists all documents in a collection
+
+   Prototype: def list_all(mongo_collection):
+   Return an empty list if no document in the collection
+   mongo_collection will be the pymongo collection object 
+"""
 
 def list_all(mongo_collection):
     if mongo_collection is not None:
