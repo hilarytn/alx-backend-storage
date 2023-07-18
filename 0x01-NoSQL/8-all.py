25294684688455afe366e27c589f97118cf18a56
Hilary Titus Naor <hilarytn@gmail.com>
2023-07-18 13:32:33 -0700

    Change conditional

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """A function that lists all documents in a collection"""
+import pymongo
+
 def list_all(mongo_collection):
-    my_list = []
-    if mongo_collection.find() is not None:
+    if mongo_collection is not None:
         docs = mongo_collection.find()
         return [doc for doc in docs]
     else:
-        return my_list
+        return []
