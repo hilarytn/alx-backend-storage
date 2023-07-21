e831e7a1d1484f68d9a899f765098bf63d337de0
Hilary Titus Naor <hilarytn@gmail.com>
2023-07-21 16:22:50 +0000

    Add some documentation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,13 +9,17 @@ from uuid import uuid4
 
 
 class Cache():
-    """This class will cache data as parsed from the store method"""
+    """ store an instance of the Redis client as a private variable 
+        named _redis (using redis.Redis()) and flush the instance using flushdb
+    """
     def __init__(self):
+        """Initialize the class"""
         import redis
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data) -> str:
+        """Create a store method that takes a data argument and returns a string. """
         id = str(uuid4())
         self._redis.set(id, data)
         return id
